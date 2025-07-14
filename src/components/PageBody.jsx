f0d3d32a8e9500c2b7763bb8e09f3d75d7b3433d
Nitin Deshmukh <nitindeshmukh1998@gmail.com>
2025-07-15 00:49:40 +0530

    added shimer

--- src/components/PageBody.jsx
+++ src/components/PageBody.jsx
@@ -2,13 +2,18 @@ import ItemCards from "./ItemCards";
 import cardData from "../utils/MockData";
 import { useState } from "react";
 import SearchBox from "./SearchBox";
-import ButtonText, { ButtonTextCostants } from "../utils/Constants"
+import { ButtonTextCostants } from "../utils/Constants";
+import Shimmer from "./Shimmer";
 const PageBody = () => {
   let newList = cardData.filter((item) => item.rating >= 4.0);
 
-  const [topRatedList, setTopRatedList] = useState(cardData);
+  const [topRatedList, setTopRatedList] = useState([]);
 
   const [buttonText, setButtonText] = useState(ButtonTextCostants.BUTTON_TEXT);
+
+  setTimeout(() => {
+    setTopRatedList(cardData);
+  }, 2000);
 
   const handleClick = () => {
     if (topRatedList === cardData) {
@@ -23,13 +28,17 @@ const PageBody = () => {
   return (
     <>
       <SearchBox handleClick={handleClick} buttonText={buttonText} />
-      <div className="body-container">
-        <div className="items-page">
-          {topRatedList.map((item) => (
-            <ItemCards key={item.idCategory} {...item} />
-          ))}
+      {topRatedList.length === 0 ? (
+        <Shimmer />
+      ) : (
+        <div className="body-container">
+          <div className="items-page">
+            {topRatedList.map((item) => (
+              <ItemCards key={item.idCategory} {...item} />
+            ))}
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 };
---
+++ src/components/Shimmer.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Shimmer = () => {
+  return (
+    <div className="shimmer-page">
+      <div className="dummy-cards"></div>
+      <div className="dummy-cards"></div>
+      <div className="dummy-cards"></div>
+      <div className="dummy-cards"></div>
+      <div className="dummy-cards"></div>
+      <div className="dummy-cards"></div>
+      <div className="dummy-cards"></div>
+      <div className="dummy-cards"></div>
+      <div className="dummy-cards"></div>
+      <div className="dummy-cards"></div>
+      <div className="dummy-cards"></div>
+      <div className="dummy-cards"></div>
+      <div className="dummy-cards"></div>
+      <div className="dummy-cards"></div>
+      <div className="dummy-cards"></div>
+      <div className="dummy-cards"></div>
+      <div className="dummy-cards"></div>
+      <div className="dummy-cards"></div>
+      <div className="dummy-cards"></div>
+      <div className="dummy-cards"></div>
+    </div>
+  );
+};
+
+export default Shimmer;
